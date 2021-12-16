fac4a5dca7036087f848e852ec8f9ee323cd31ec
Paulo César <93353706+pdossantosjunior@users.noreply.github.com>
2021-12-16 17:10:00 -0300

    Lua 16-12-2021
    Lua 16-12-2021

--- LUA/Teste.lua
+++ LUA/Teste.lua
@@ -50,8 +50,30 @@ textoB = 'World'
 print(textoA .. textoB) -- .. concatena os textos
 print(textoA .. " " .. textoB) -- tanto "" quanto '' servem para mostrar que é texto
 
+print(math.ceil(10.5)) -- pacote (biblioteca) matemática para tratar número | no caso de ceil arredonda para mais
 
+print(string.len('hello')) -- pacote (biblioteca) que informa o tamanho da string
+print(#'hello') -- tem o mesmo efeito do comando acima
 
+print(string.sub('hello', 1, 2)) -- pacote (biblioteca) que imprime apenas os caracteres selecionados | 1 e 2 são os intervalos - no caso de 1 e 4, imprimirá 'hell' - pode começar de outros além do 1
+
+print(string.upper('hello')) -- coloca todas as letras em caixa alta
+
+text = 'helLO'
+
+first = string.sub(text, 1, 1)
+last = string.sub(text, 2, #text)
+
+print(
+    string.upper(first) ..
+    string.lower(last))
+
+textrep = 'Hello'
+margin = 10 -- Definir a quantidade de vezes que vai repetir
+
+print (string.rep(' ', margin) .. textrep) -- string.rep(A,5) vai repetir a letra 'A' 5 vezes
+
+print(string.reverse('ABCDEFGHIJK')) -- serve para reverter a escrita
 
 
 
