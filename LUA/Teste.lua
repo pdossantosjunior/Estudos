b909f58ad6517f6823dbdacc6faddb93b331f48b
Paulo César <93353706+pdossantosjunior@users.noreply.github.com>
2021-12-17 20:35:47 -0300

    Lua 17-12-2021
    Lua 17-12-2021

--- LUA/Teste.lua
+++ LUA/Teste.lua
@@ -76,4 +76,65 @@ print (string.rep(' ', margin) .. textrep) -- string.rep(A,5) vai repetir a letr
 print(string.reverse('ABCDEFGHIJK')) -- serve para reverter a escrita
 
 
+email = 'store@gmail.com'
+
+from, rall = string.find(email, 'gmail') -- a função find busca um texto dentro de outro texto, no caso usei as variáveis from e rall para armazenar a resposta do find, onde a primeira armazena onde começa e a segunda onde termina
+
+print(from, " ", rall)
+
+
+-- Imprimindo o provedor de e-mail
+
+email = 'hello@yahoo.com.br'
+
+at = string.find(email, '@')
+
+if at == nil then
+    print('Invalid email')
+else
+    provider = string.sub(email, at + 1, #email) -- at + 1 é para não imprimir o @ junto
+
+    print('Provider: ' .. provider)
+end
+
+-- Substituindo palavras ou letras
+
+text = 'A cidade de Araraquara é a melhor cidade!'
+
+print(string.gsub(text, 'cidade', 'city'))
+
+print(string.gsub(text, 'a', '@'))
+
+print(string.gsub(text, 'a', '@', 2)) -- o número 2 informa até qual ocorrência será substituido 
+
+-- Array / Lista
+
+list = {4, 7, 8}
+
+table.insert(list, 72) -- dessa forma o número 72 será adicionado no array seguindo o conceito de lista e por isso ele estará depois do valor 8
+
+print(list[4]) -- em Lua o array começa em 1 e não em 0
+
+
+text = 'terra'
+number = 7
+
+list = { text, number, 5} -- é possível jogar os valores de outras variáveis no array, porém isso não vincula o conteúdo de uma com a outra
+
+print(list[1])
+print(list[2])
+print(list[3])
+
+
+
+list = {10, 20, 30, 40, 50}
+
+table.insert(list, 60)
+
+for index, value in pairs(list) do -- neste caso o 'in pairs' vai imprimir todo o array/lista
+    print(index .. " : " .. value)
+end
+
+
+
 
